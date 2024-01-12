ecc2ff2f964a02e652035f7d67dbd3b793e50796
Ark Fen <arkfen@gmail.com>
2024-01-12 12:31:37 -0500

    IBank code implemented

--- BankLib/IBank.cs
+++ BankLib/IBank.cs
@@ -1,6 +1,11 @@
-﻿namespace BankLib;
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace BankLib;
 
 public interface IBank
 {
-
+    string Name { get; }
+    List<IAccount> Accounts { get; set; }
+    void SetName(string name);
+    void AddAccount(IAccount account);
 }
