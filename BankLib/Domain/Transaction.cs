6762903796c8ea8536cd29233db057a72e97afac
Ark Fen <arkfen@gmail.com>
2024-01-12 16:21:10 -0500

    Transaction class added

---
+++ BankLib/Domain/Transaction.cs
@@ -0,0 +1,19 @@
+﻿namespace BankLib;
+
+public class Transaction : ITransaction
+{
+    public void Deposit(string accountNumber, decimal amount)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Transfer(string senderAccNum, string beneficiaryAccNum, decimal amount)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Withdaw(string accountNumber, decimal amount)
+    {
+        throw new NotImplementedException();
+    }
+}
