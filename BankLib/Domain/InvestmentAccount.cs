d66ab685a812d2250338f0d17e691dc2bb5fd8df
Ark Fen <arkfen@gmail.com>
2024-01-12 16:07:23 -0500

    InvestmentAccount class implemented

---
+++ BankLib/Domain/InvestmentAccount.cs
@@ -0,0 +1,36 @@
+﻿namespace BankLib;
+
+public class InvestmentAccount(string number, IClient owner) : Account
+{
+    enum AccType
+    {
+        Individual,
+        Corporate
+    }
+
+    public override string Number { get; } = number;
+    public override IClient Owner { get; } = owner;
+    AccType Type
+    {
+        get
+        {
+            return type;
+        }
+        set
+        {
+            if (Owner.GetType() == typeof(IndividualClient))
+            {
+                type = AccType.Individual;
+                WithdrawLimit = 500;
+            }
+            if (Owner.GetType() == typeof(CorporateClient))
+            {
+                type = AccType.Corporate;
+            }
+        }
+    }
+    private AccType type;
+
+
+
+}
