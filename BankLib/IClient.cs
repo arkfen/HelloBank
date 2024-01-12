6cc68b948ca89468eb00c121744f2964724eee88
Ark Fen <arkfen@gmail.com>
2024-01-12 12:56:52 -0500

    Client and Trsnasction interfaces code implemented

--- BankLib/IClient.cs
+++ BankLib/IClient.cs
@@ -2,5 +2,6 @@
 
 public interface IClient
 {
-
+    string Address { get; set; }
+    string Telephone { get; set; }
 }
--- BankLib/ITransaction.cs
+++ BankLib/ITransaction.cs
@@ -2,5 +2,7 @@
 
 public interface ITransaction
 {
-
+    void Deposit(string accountNumber, decimal amount);
+    void Withdaw(string accountNumber, decimal amount);
+    void Transfer(string senderAccNum, string beneficiaryAccNum, decimal amount);
 }
