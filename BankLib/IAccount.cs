9f295d97f4b5158e24ded39ea104f6eca2f3c3ab
Ark Fen <arkfen@gmail.com>
2024-01-12 12:40:04 -0500

    IAccount code implemented

--- BankLib/IAccount.cs
+++ BankLib/IAccount.cs
@@ -1,6 +1,11 @@
-﻿namespace BankLib;
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace BankLib;
 
 public interface IAccount
 {
-
+    IClient Owner { get; set; }
+    Decimal Balance { get; set; }
+    Decimal WithdrawLimit { get; set; }
+    void InitDeposit(Decimal amount);
 }
