51908853425cd3399b6daebbe1d4945de2e4dfc3
Ark Fen <arkfen@gmail.com>
2024-01-12 19:44:15 -0500

    Comments added to Transaction class methods

--- BankLib/Domain/Transaction.cs
+++ BankLib/Domain/Transaction.cs
@@ -2,16 +2,32 @@
 
 public class Transaction : ITransaction
 {
+    /// <summary>
+    /// Depositing funds to the account
+    /// </summary>
+    /// <param name="accountNumber">Account number</param>
+    /// <param name="amount">Amount to deposit</param>
     public static async Task DepositAsync(string accountNumber, decimal amount)
     {
         throw new NotImplementedException();
     }
 
+    /// <summary>
+    /// Transfering funds from one account to another
+    /// </summary>
+    /// <param name="senderAccNum">Sender Account</param>
+    /// <param name="beneficiaryAccNum">Beneficiary Account</param>
+    /// <param name="amount">Amount to transfer</param>
     public static async Task TransferAsync(string senderAccNum, string beneficiaryAccNum, decimal amount)
     {
         throw new NotImplementedException();
     }
 
+    /// <summary>
+    /// Withdraing specific amount from the bank account
+    /// </summary>
+    /// <param name="accountNumber">Account Number to withdraw from</param>
+    /// <param name="amount">Amount to withdraw</param>
     public static async Task WithdawAsync(string accountNumber, decimal amount)
     {
         throw new NotImplementedException();
