631cd54a672fc227f68eede78651d471c2687ed4
Ark Fen <arkfen@gmail.com>
2024-01-12 19:40:52 -0500

    Comments added to the AddAccount method

--- BankLib/Domain/Bank.cs
+++ BankLib/Domain/Bank.cs
@@ -6,6 +6,11 @@ public class Bank(string name) : IBank
 
     public List<IAccount> Accounts { get; } = [];
 
+    /// <summary>
+    /// Adding new bank account to the Bank with unique account number
+    /// </summary>
+    /// <param name="account">Account Object</param>
+    /// <returns>Completed Task (void)</returns>
     public async Task AddAccountAsync(IAccount account)
     {
         if (Accounts.Count(x => x.Number == account.Number) > 0)
